7b0913f76dec97850f19284a555a5e819ff77b5c
pogorelov-evgen <72943262+pogorelov-evgen@users.noreply.github.com>
2023-12-06 16:34:41 +0300

    first test hibernate

--- testing/src/Main.java
+++ testing/src/Main.java
@@ -1,51 +1,17 @@
-import java.util.Random;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.BlockingQueue;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-public class Main {
-    private static BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
+public class Main{
     public static void main(String[] args) throws InterruptedException {
-
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    produce();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    consumer();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-        thread1.start();
-        thread2.start();
-
-        thread1.join();
-        thread2.join();
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list = list.stream().map(x->x*2).toList();
+        list = list.stream().filter(x->x%2==0).map(x->x*2).map(x->2).toList();
 
 
-    }
-    private static void produce() throws InterruptedException {
-        Random random = new Random();
-        while (true) {
-            queue.put(random.nextInt(10));
-        }
-    }
-    private static void consumer() throws InterruptedException {
-        while (true) {
-            Thread.sleep(1000);
-            System.out.println("Я выполнил работу № " + queue.take());
-            System.out.println("Размер очереди составает " + queue.size());
-        }
+
     }
 }
